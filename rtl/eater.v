ffc05a938a885ae88f829e3dad7d0d6a5e5a9e15
CorvusPrudens <corvusprudens@gmail.com>
2022-12-28 11:56:27 -0800

    Added a handful of explanatory comments

--- rtl/eater.v
+++ rtl/eater.v
@@ -29,6 +29,12 @@ module eater #(
     // Control Signals
     ///////////////////////////////
 
+    // This is a technique called double-flopping. It helps
+    // prevent glitches when a signal crosses what you might
+    // call a "clock domain" (i.e. cross-domain-clocking).
+    // Since the reset input would likely be a button,
+    // it's not syncronized to the system clock, and
+    // should therefore be double-flopped.
     reg [1:0] reset_cdc;
     always @(posedge clk_i) begin
         reset_cdc[0] <= reset_i;
@@ -77,6 +83,10 @@ module eater #(
     reg [7:0] a_reg;
     reg [7:0] b_reg;
 
+    // A 9-bit sum register allows us to easily get at the
+    // carry-out signal of this adder (sum_reg[8]). It also 
+    // means we need to use 9-bit operands if we want to be
+    // totally explicit.
     wire [8:0] sum_reg = c_subtract   ? 
         {1'b0, a_reg} - {1'b0, b_reg} :
         {1'b0, a_reg} + {1'b0, b_reg} ;
@@ -102,6 +112,10 @@ module eater #(
             flag_zero  <= 1'b0;
             flag_carry <= 1'b0;
         end else if (c_flags_in) begin
+            // One of the nice features of verilog is the
+            // ability to describe logic in a behavioral way.
+            // Rather than describing chains of OR operations,
+            // we can simply make a higher-level arithmetic assertion.
             flag_zero  <= sum_reg[7:0] == 0;
             flag_carry <= sum_reg[8];
         end
@@ -116,6 +130,10 @@ module eater #(
 
     reg [7:0] ram_output;
 
+    // Verilator doesn't like coercing a string into a
+    // boolean expression, so the width warning is turned off.
+    // yosys can have issues with synthesizing ROMs if the
+    // module doesn't have a valid path in its file.
     // verilator lint_off WIDTH
     initial if (PROGRAM_HEX) $readmemh(PROGRAM_HEX, ram);
     // verilator lint_on WIDTH
@@ -135,6 +153,11 @@ module eater #(
     ///////////////////////////////
     // Output
     ///////////////////////////////
+
+    // This block mimics the behavior of Ben Eater's
+    // muxed display, with the addition of a
+    // clock divider in case the system clock is
+    // in the MHz.
 
     reg [7:0] output_reg;
     reg [3:0] output_com_reg;
@@ -216,6 +239,9 @@ module eater #(
         end
     end
 
+    // The halt signal is separately registered so that
+    // any change to the control state machine (and instruction address)
+    // does not immediately deassert the halt.
     always @(posedge clk_i) begin
         if (reset)
             halt <= 1'b0;
@@ -223,6 +249,8 @@ module eater #(
             halt <= 1'b1;
     end
 
+    // In Verilog, vectors like this can be manipulated
+    // like regular variables, which is really nice.
     assign {
         c_sum_out,
         c_subtract,
@@ -245,6 +273,11 @@ module eater #(
     ///////////////////////////////
     // Bus Management
     ///////////////////////////////
+
+    // This ORed mux mostly mimics the behavior of the
+    // Ben Eater bus, excluding actual tri-state interactions.
+    // Tri-state buffers are generally not supported within the
+    // fabric of an FPGA.
     assign bus =
         (c_ram_out         ? ram_output                   : 8'b0) |
         (c_instruction_out ? {4'b0, instruction_reg[3:0]} : 8'b0) |
